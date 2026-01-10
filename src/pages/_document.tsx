f65947931a69985142d540b3eb2342de75a94a0b
softgenai <admin@softgen.ai>
2026-01-10 19:23:31 +0000

    feat(config): Add WWW to non-WWW redirect for `problemlot.com`

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,14 +5,32 @@ export default function Document() {
   return (
     <Html lang="pl">
       <Head>
-        <SEOElements 
-          title="Opóźniony lub odwołany lot – sprawdź odszkodowanie do 600 €"
-          description="Masz problem z lotem? Sprawdź, czy przysługuje Ci nawet 600 € odszkodowania za opóźniony lub odwołany lot."
-          url="https://problemlot.com"
+        <SEOElements />
+        
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KD4RSD4F');`,
+          }}
         />
-        <link rel="canonical" href="https://problemlot.com" />
+        {/* End Google Tag Manager */}
       </Head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KD4RSD4F"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <Main />
         <NextScript />
       </body>
